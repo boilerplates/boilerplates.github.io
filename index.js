25c79a0a28c6bdd506167c8359646faa049526fb
Brian Woodward <brian.woodward@gmail.com>
2015-09-02 14:11:24 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * boilerplates-github-io <https://github.com/boilerplates/boilerplates-github-io>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
